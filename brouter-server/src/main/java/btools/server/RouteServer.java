c3771b5fed562308cf0877d455c1fc2b18fdb870
Henrique Malheiro <henrique.malheiro@gmail.com>
2021-10-31 22:37:19 +0000

    Logging

--- brouter-server/src/main/java/btools/server/RouteServer.java
+++ brouter-server/src/main/java/btools/server/RouteServer.java
@@ -331,9 +331,9 @@ public class RouteServer extends Thread implements Comparable<RouteServer>
 	      } catch (Exception ignore) {
 	      }
               cr = null;
-              if ( br != null ) try { br.close(); } catch( Exception e ) {}
-              if ( bw != null ) try { bw.close(); } catch( Exception e ) {}
-              if ( clientSocket != null ) try { clientSocket.close(); } catch( Exception e ) {}
+//              if ( bw != null ) try { bw.close(); } catch( Exception e ) {}
+//            if ( br != null ) try { br.close(); } catch( Exception e ) {}
+            if ( clientSocket != null ) try { clientSocket.close(); } catch( Exception e ) {}
               terminated = true;
               synchronized( threadPoolSync )
               {
