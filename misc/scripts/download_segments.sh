ed7a9b0f61e59f7a6ed795750faf76e9189486ec
Henrique Malheiro <henrique.malheiro@gmail.com>
2021-11-01 01:07:00 +0000

    Update to download segments script

--- misc/scripts/download_segments.sh
+++ misc/scripts/download_segments.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-mkdir ~/misc/segments4
-cd ~/misc/segments4
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+SEGMENTS_DIR=${SCRIPT_DIR}/../segments4
+
+mkdir ${SEGMENTS_DIR}
+cd ${SEGMENTS_DIR}
 wget http://brouter.de/brouter/segments4/W10_N35.rd5
 wget http://brouter.de/brouter/segments4/W10_N40.rd5
